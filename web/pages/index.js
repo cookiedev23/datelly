4eacfa5ae2425474763094175aab2ba8787b4a9e
cookiedev23 <cookiedeveloper23@gmail.com>
2022-05-31 15:19:28 +0200

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,5 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Non ce un cazzo coglione ispettore</p>
+      <p>
+        Per ora sta solo qualcosa in "/appointments" scrivi cosi accanto all url
+      </p>
+    </div>
+  );
 }
